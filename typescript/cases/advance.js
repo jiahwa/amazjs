2f2c326619c73054a12777eb4c378ec8929a72a3
yu.jh <yujahua@163.com>
2020-08-05 09:38:42 +0800

    Add typescript exercises 15

--- typescript/cases/advance.js
+++ typescript/cases/advance.js
@@ -1,2 +1,9 @@
 // way 2: assign the object to another variable
 var squareOptions = { colour: "red", width: 100 };
+// Indexable Types, array index signature
+// String index structrues, is good at describing "dictionary" pattern
+// 我们还可以使用带有调用签名的对象字面量来定义泛型函数
+function identity(arg) {
+    return arg;
+}
+var myIdentity = identity;
--- typescript/cases/advance.ts
+++ typescript/cases/advance.ts
@@ -13,3 +13,10 @@ interface SearchFunc {
 
 // Indexable Types, array index signature
 // String index structrues, is good at describing "dictionary" pattern
+
+// 我们还可以使用带有调用签名的对象字面量来定义泛型函数
+function identity<T>(arg: T): T {
+    return arg;
+}
+
+let myIdentity: {<T>(arg: T): T} = identity;
