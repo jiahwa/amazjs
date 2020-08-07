3b0d55f0abdd9ffea08f961fbcc10418c69d7de0
yu.jh <yujahua@163.com>
2020-08-07 18:24:27 +0800

    complete exercise 5

--- typescript/typescript-exercises-master/exercises/exercise-05/index.ts
+++ typescript/typescript-exercises-master/exercises/exercise-05/index.ts
@@ -64,11 +64,14 @@ function logPerson(person: Person) {
     );
 }
 
-function filterPersons(persons: Person[], personType: string, criteria: unknown): unknown[] {
+function filterPersons(persons: Person[], personType: 'user', criteria: Partial<User>): User[];
+function filterPersons(persons: Person[], personType: 'admin', criteria: Partial<Admin>): Admin[];
+function filterPersons(persons: Person[], personType: string, criteria: Partial<Person>): unknown[] {
     return persons
         .filter((person) => person.type === personType)
         .filter((person) => {
-            let criteriaKeys = Object.keys(criteria) as (keyof Person)[];
+            let criteriaKeys = getObjectKeys(criteria);
+            //Object.keys(criteria) as (keyof Person)[];
             return criteriaKeys.every((fieldName) => {
                 return person[fieldName] === criteria[fieldName];
             });
@@ -86,5 +89,44 @@ console.log();
 console.log(chalk.yellow('Admins of age 23:'));
 adminsOfAge23.forEach(logPerson);
 
+type Partial<T> = {
+    [P in keyof T]?: T[P];
+}
+
+function getObjectKeys(criteria: Partial<Person>){
+    return Object.keys(criteria) as (keyof Person)[];
+}
+
 // In case if you are stuck:
 // https://www.typescriptlang.org/docs/handbook/functions.html#overloads
+
+
+let suits = ["hearts", "spades", "clubs", "diamonds"];
+
+function pickCard(x: { suit: string; card: number }[]): number;
+function pickCard(x: number): { suit: string; card: number };
+function pickCard(x: any): any {
+  // Check to see if we're working with an object/array
+  // if so, they gave us the deck and we'll pick the card
+  if (typeof x == "object") {
+    let pickedCard = Math.floor(Math.random() * x.length);
+    return pickedCard;
+  }
+  // Otherwise just let them pick the card
+  else if (typeof x == "number") {
+    let pickedSuit = Math.floor(x / 13);
+    return { suit: suits[pickedSuit], card: x % 13 };
+  }
+}
+
+let myDeck = [
+  { suit: "diamonds", card: 2 },
+  { suit: "spades", card: 10 },
+  { suit: "hearts", card: 4 },
+];
+
+let pickedCard1: number = pickCard(myDeck);
+// alert("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+
+let pickedCard2: object = pickCard(15);
+// alert("card: " + pickedCard2.card + " of " + pickedCard2.suit);
