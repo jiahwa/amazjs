1f216ba18ec9afe3e59afb9349dbeeb71f202260
yu.jh <yujahua@163.com>
2021-05-13 10:36:48 +0800

    feature(export,import)

---
+++ es6-babel/src/export.testing.js
@@ -0,0 +1,8 @@
+export const abc = () => false
+const A = 'a'
+const B = 'b'
+export {
+    A,
+    B
+}
+export default true
---
+++ es6-babel/src/import.testing.js
@@ -0,0 +1,7 @@
+import {abc} from './export.testing'
+import  * as ex  from './export.testing'
+import expor from './export.testing'
+
+console.dir(abc)
+console.dir(ex)
+console.dir(expor)
